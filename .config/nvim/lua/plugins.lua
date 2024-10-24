25562a4cfd338c74f65ca1468492931d0960c003
cxreiff <cooper@cxreiff.com>
2024-10-24 16:22:53 -0700

    added leetcode plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -77,6 +77,21 @@ require('lazy').setup({
     config = require('setup/lsp_zero'),
   },
 
+  -- leetcode
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    lazy = "leet" ~= vim.fn.argv()[1],
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = require('setup/leetcode'),
+  },
+
   -- languages
   { 'rust-lang/rust.vim', ft = 'rust' },
   { 'kaarmu/typst.vim', ft = 'typst' },
---
+++ .config/nvim/lua/setup/leetcode.lua
@@ -0,0 +1,4 @@
+return {
+  arg = "leet",
+  lang = "javascript",
+}
